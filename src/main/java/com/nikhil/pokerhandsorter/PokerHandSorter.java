738c96f9f136b3ed91cad4d15240856e0cbc7318
Nikhil Murthy <Nikhil@Macs-MacBook-Air-2.local>
2019-05-09 18:21:23 +1000

    added dependencies and commiting pom file

---
+++ src/main/java/com/nikhil/pokerhandsorter/PokerHandSorter.java
@@ -0,0 +1,20 @@
+package com.nikhil.pokerhandsorter;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class PokerHandSorter {
+	
+	public static void main (String[] args) {
+		
+//		try {
+//			BufferedReader pokerHandsReader = new BufferedReader(new InputStreamReader(System.in));
+//		} catch (Exception exp) {
+//			System.out.println(exp.getMessage());
+//		}
+		 
+		
+		System.out.println("here");
+	}
+
+}
